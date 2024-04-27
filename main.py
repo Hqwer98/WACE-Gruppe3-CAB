8823fed7f8c5eecd5755034228075126d9313066
EL-W00D <christian.rosenhagen@icloud.com>
2024-04-27 14:20:06 +0200

    Ableitung an einem Punkt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import Numpy as np
+import numpy as np
 
 test = 123
 print(test)
@@ -15,3 +15,12 @@ def apply(x):
 
 def f(x):
     return np.square(x) + 1
+
+
+x_point = 2
+
+delta_x = 1
+
+f_ableitung = (f(x_point + delta_x) - f(x_point) / delta_x)
+
+print("Ableitung von f(x) an der Stelle x=", x_point, "ist", f_ableitung)
