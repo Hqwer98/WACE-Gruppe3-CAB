cfda0f736661db7d933005f970d0a8183202e413
Hqwer98 <116880146+Hqwer98@users.noreply.github.com>
2024-05-20 23:30:15 +0200

    plotten der Funktionen

--- Code/Derivative.py
+++ Code/Derivative.py
@@ -8,7 +8,7 @@ class Derivative:
         Calculates the first derivative of f with respect to x
         :param f: function f(x)
         :param x: x value
-        :param h: distance around x
+        :param h: approximation value around x
         :return: the approximate derivative of f with respect to x
         """
         return (f(x + h) - f(x - h)) / (2 * h)
@@ -19,7 +19,7 @@ class Derivative:
         Calculates the second derivative of f with respect to x
         :param f: function f(x)
         :param x: x value
-        :param h: distance around x
+        :param h: approximation value around x
         :return: the approximate second derivative of f with respect to x
         """
         return (f(x + h) - 2 * f(x) - f(x - h)) / (np.square(h))
--- Code/main.py
+++ Code/main.py
@@ -1,17 +1,21 @@
 import numpy as np
-
-test = 1234
-print(test)
-
-print("Hello World!")
-print("moin moin meister nase")
-
-print("Test-C")
-
-
-def apply(x):
-    return x / 1
+import matplotlib.pyplot as plt
+from Derivative import Derivative
 
 
 def f(x):
-    return np.square(x) + 1
+    return 0.2 * np.power(x, 3) + x
+
+
+x = np.linspace(-4, 4, 100)
+f0 = f(x)
+h = 0.01
+
+f1 = Derivative.of_order_1(f, x, h)
+
+plt.plot(x, f0, label='f(x)')
+plt.plot(x, f1, label='f´(x)')
+plt.legend()
+plt.title('Erste Ableitung der Funktion f')
+plt.grid()
+plt.show()
