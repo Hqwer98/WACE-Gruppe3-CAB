f3d7da29f9be8149ecd56b3f2d9058c69188a9ca
Hqwer98 <116880146+Hqwer98@users.noreply.github.com>
2024-05-20 22:36:39 +0200

    Ableitungsklasse erstellt

---
+++ Code/Derivative.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+class Derivative:
+    @staticmethod
+    def of_order_1(f, x, h):
+        """
+        Calculates the first derivative of f with respect to x
+        :param f: function f(x)
+        :param x: x value
+        :param h: distance around x
+        :return: the approximate derivative of f with respect to x
+        """
+        return (f(x + h) - f(x - h)) / (2 * h)
+
+    @staticmethod
+    def of_order_2(f, x, h):
+        """
+        Calculates the second derivative of f with respect to x
+        :param f: function f(x)
+        :param x: x value
+        :param h: distance around x
+        :return: the approximate second derivative of f with respect to x
+        """
+        return (f(x + h) - 2 * f(x) - f(x - h)) / (np.square(h))
