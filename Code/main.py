50913b1b19cfd1d51716ce31bf31d145b7fb927c
Hqwer98 <116880146+Hqwer98@users.noreply.github.com>
2024-05-21 12:19:01 +0200

    added plotting of functions

--- Code/main.py
+++ Code/main.py
@@ -1,21 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from Derivative import Derivative
+from typing import Callable
 
 
-def f(x):
-    return 0.2 * np.power(x, 3) + x
+def f(x: np.ndarray) -> np.ndarray:
+    """
+    Defines function f
+    :param x: x value
+    :return: the function (1/27)*x^3
+    """
+    return 1 / 27 * np.power(x, 3)
 
 
-x = np.linspace(-4, 4, 100)
-f0 = f(x)
-h = 0.01
+def plot_function_with_derivatives(function: Callable[[np.ndarray], np.ndarray], i: float, function_expression: str):
+    x = np.linspace(-i, i, 100)
+    f = function(x)
+    h = 0.01
 
-f1 = Derivative.of_order_1(f, x, h)
+    df = Derivative.of_order_1(function, x, h)
+    ddf = Derivative.of_order_2(function, x, h)
 
-plt.plot(x, f0, label='f(x)')
-plt.plot(x, f1, label='f´(x)')
-plt.legend()
-plt.title('Erste Ableitung der Funktion f')
-plt.grid()
-plt.show()
+    plt.plot(x, f, label=r'$f(x)$')
+    plt.plot(x, df, label=r'$f´(x)$')
+    plt.plot(x, ddf, label=r'$f´´(x)$')
+    plt.legend()
+    plt.title('Funktion ' + function_expression + ' mit erste und zweite Ableitung')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid()
+    plt.show()
+
+
+plot_function_with_derivatives(f, 4, r'f(x) = $\frac{1}{27}x^3$')
