70ae4d2090025384a644e4a2a046319ed7bd4270
Hqwer98 <116880146+Hqwer98@users.noreply.github.com>
2024-06-25 15:35:38 +0200

    preparing Integral Test

--- Code/Approximation_Integral/Integral_Test.py
+++ Code/Approximation_Integral/Integral_Test.py
@@ -2,34 +2,11 @@ from typing import Callable
 
 import numpy as np
 import matplotlib.pyplot as plt
-from Code.Approximation_Integral.Newton_Cotes.NewtonCotes import NewtonCotes
-from Code.Approximation_Integral.Newton_Cotes.TrapezoidalRule import TrapezoidalRule
 from Code.Approximation_Integral.Newton_Cotes.SimpsonRule import SimpsonRule
 
 
 def f(x):
     return 4 * np.power(x, 3) + 2 * x
-
-
-def g(x):
-    return np.sin(x)
-
-
-def test_approximation(function: Callable, a: float, b: float):
-    trapezoidalRule = TrapezoidalRule()
-    simpsonRule = SimpsonRule()
-    rule1 = NewtonCotes(3)
-    rule2 = NewtonCotes(4)
-
-    trapezoidal_composite_result = trapezoidalRule.calculate_integral_composite(function, a, b)
-    simpson_simple_result = simpsonRule.calculate_integral_simple(function, a, b)
-    newton_cotes_result1 = rule1.calculate_integral(function, a, b)
-    newton_cotes_result2 = rule2.calculate_integral(function, a, b)
-
-    print("Trapez-Regel Ergebnis:", trapezoidal_composite_result)
-    print("Simpson-Regel Ergebnis:", simpson_simple_result)
-    print("3/8-Regel Ergebnis:", newton_cotes_result1)
-    print("Milne-Regel Ergebnis:", newton_cotes_result2)
 
 
 def plot_function_with_integral(function: Callable, a: float, b: float, node_count=4):
@@ -63,4 +40,3 @@ def plot_function_with_integral(function: Callable, a: float, b: float, node_cou
 
 
 plot_function_with_integral(f, 0, 1)
-test_approximation(g, 0, np.pi)
