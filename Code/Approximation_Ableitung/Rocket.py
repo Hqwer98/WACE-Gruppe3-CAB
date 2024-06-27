8097f6c45a41fea9daa25c4ad7a615b8d2494c73
Hqwer98 <116880146+Hqwer98@users.noreply.github.com>
2024-06-27 12:06:37 +0200

    made intervall smaller

--- Code/Approximation_Ableitung/Rocket.py
+++ Code/Approximation_Ableitung/Rocket.py
@@ -14,22 +14,34 @@ def f(x):
 h = 1
 ra = Derivative.of_order_1(f, rt, h)
 
+rocket_plotting_time = []
+rocket_plotting_height = []
+rocket_plotting_velocity = []
+rocket_plotting_acceleration = []
+
+for i in range(len(rt)):
+    if rt[i] <= 200:
+        rocket_plotting_time.append(rt[i])
+        rocket_plotting_height.append(rh[i])
+        rocket_plotting_velocity.append(rv[i])
+        rocket_plotting_acceleration.append(ra[i])
+
 fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10, 8))
 
-ax1.plot(rt, rh, 'o', label='Raketen Höhe')
-ax1.plot(rt, rh)
+ax1.plot(rocket_plotting_time, rocket_plotting_height, 'o', label='Raketen Höhe')
+ax1.plot(rocket_plotting_time, rocket_plotting_height)
 ax1.legend(loc='best')
 ax1.grid()
 ax1.set_ylabel(r"Höhe in $m$")
 
-ax2.plot(rt, rv, 'o', label='Raketen Geschwindigkeit')
-ax2.plot(rt, rv)
+ax2.plot(rocket_plotting_time, rocket_plotting_velocity, 'o', label='Raketen Geschwindigkeit')
+ax2.plot(rocket_plotting_time, rocket_plotting_velocity)
 ax2.legend(loc='best')
 ax2.grid()
 ax2.set_ylabel(r"Geschwindigkeit in $m/s$")
 
-ax3.plot(rt, ra, 'o', label='Raketen Beschleunigung')
-ax3.plot(rt, ra)
+ax3.plot(rocket_plotting_time, rocket_plotting_acceleration, 'o', label='Raketen Beschleunigung')
+ax3.plot(rocket_plotting_time, rocket_plotting_acceleration)
 ax3.legend(loc='best')
 ax3.grid()
 ax3.set_ylabel(r"Beschleunigung in $m/s^2$")
